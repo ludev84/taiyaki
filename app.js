5dc7884da07a6253ed0934225dc37caeda0ba4ac
Luis Fernando Cardeña Ley <luis.fer160197@gmail.com>
2022-09-04 22:52:18 -0500

    transition fixed

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+let timeoutId;
+
 const poses = [
   [3600 ,"assets/Ayu1.gif"],
   [19000 ,"assets/Ayu2.gif"]
 ]
+
 $("#gif1").on('click', () => {
+  console.log(timeoutId);
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+
   const lenPoses = poses.length;
   let rand = Math.ceil(Math.random()*lenPoses) -1;
   $("#gif1").attr("src", poses[rand][1]);
-  setTimeout( () => {
+  timeoutId = setTimeout( () => {
       $("#gif1").attr("src", 'assets/Ayu_IDLE.gif');
   }, poses[rand][0]);
 });
